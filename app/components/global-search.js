2e11408469dd3d421a08760c8eb1ceb9a6c5ed68
Tymm Schmitke <ifellinaholeonce@gmail.com>
2018-11-27 23:30:13 -0500

    AJAX Request to Search

--- app/components/global-search.js
+++ app/components/global-search.js
@@ -1,16 +1,34 @@
 import Component from '@ember/component';
 import {
+  inject
+} from '@ember/service'
+import {
   debounce
 } from '@ember/runloop';
 
 export default Component.extend({
+  ajax: inject(),
   actions: {
     q() {
+      this.set('results', false)
+      this.set('pending', true)
       let query = { q: this.get('q') }
-      let myQ = () => {
-        console.log(this.q)
+      let sendQuery = () => {
+        if (query.q === '') {
+          this.set('pending', false)
+          return
+        }
+        this.get('ajax').request('/search', {
+          method: 'GET',
+          data: query
+        }).then(response => {
+          this.set('pending', false)
+          this.set('results', true)
+          this.set('books', response.books)
+          this.set('authors', response.authors)
+        })
       }
-      debounce( query, myQ, 200)
+      debounce( query, sendQuery, 150)
     }
   }
 });
